7d26e529e5d0c3785b0227f5ce20076d0356a370
West Kraemer <kraemerwest@gmail.com>
2018-08-01 16:28:14 -0400

    add spinner to home screen indicating to user that next VC is loading.

--- Met Gallery/HomeVC.swift
+++ Met Gallery/HomeVC.swift
@@ -13,7 +13,17 @@ class HomeVC: UIViewController {
     override func viewDidLoad() { super.viewDidLoad() }
        
         @IBAction func launchArtGallery(_ sender: Any) {
+            let galleryVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "GalleryVC") as! GalleryVC
+            let activityIndicatorView = UIActivityIndicatorView()
+            activityIndicatorView.activityIndicatorViewStyle = .whiteLarge
+            activityIndicatorView.center = CGPoint(x: view.center.x, y: view.center.y)
+            activityIndicatorView.startAnimating()
+            view.addSubview(activityIndicatorView)
             
-        }
+            present(galleryVC, animated: true, completion: {
+                activityIndicatorView.stopAnimating()
+                activityIndicatorView.removeFromSuperview()
+            })
+    }
     
 }
